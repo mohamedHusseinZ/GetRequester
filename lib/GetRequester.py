6d56667eef25a6a54c7031f5c9ccde165efb07df
masud5201~ <your.email@masud.abdi@student.moringaschool.com>
2024-02-02 12:58:02 -0800

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,17 @@
 import requests
 import json
 
-class GetRequester:
 
+import requests
+
+class GetRequester:
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.text
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
